e90993cb3431894a362957fe79649c98d42a174f
hypermug1 <63418967+hypermug1@users.noreply.github.com>
2021-04-30 10:12:55 -0400

    Create gamma.sh

---
+++ gamma.sh
@@ -0,0 +1,1 @@
+cd ~ && wget https://gist.githubusercontent.com/hypermug1/77300da4fcad74db1b38010c3a64bf44/raw/f5b547130538cca40e7023def6d43931afebffec/gamma.desktop && sudo cp gamma.desktop /etc/xdg/autostart/gamma.desktop && echo "You may now edit the file to add whatever you want." && sleep 5 && rm ~/gamma.desktop && sudo nano /etc/xdg/autostart/gamma.desktop
